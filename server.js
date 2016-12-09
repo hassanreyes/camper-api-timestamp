9919b0ad7a623cff77846cd0d07a2f0f2acaec5d
Hassan <hassan.reyes@gmail.com>
2016-12-09 22:03:40 +0000

    timestamp micro service first aproach

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-var express = require('express')
-var app = express()
+var express = require('express'),
+    app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/:date', (req, res) => {
+  
+  var date = req.params.date;
+  var options = { year: 'numeric', month: 'long', day: 'numeric' };
+  var ts;
+  
+  if(/^\d+$/.test(date)){
+    ts = new Date(Number(date));
+  }else{
+    ts = new Date(Date.parse(date));
+  }
+  
+  var ms = ts.getTime();
+  
+  res.json({ unix: ms, natural: Number.isNaN(ms) ? null : ts.toLocaleDateString('en-US', options) });
+});
+
+app.get("*", (req, res) =>{
+  res.send("<p>Please pass a date parameter in the URL.</p>");
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
